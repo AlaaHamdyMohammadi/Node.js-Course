e34f124900eba7f871bb607891424379cd3c39a4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-08 19:50:10 +0200

    update

--- Section2(node-farm)/starter/index.js
+++ Section2(node-farm)/starter/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
 const fs = require('fs');
-const hello = 'Hello Node.js';
-console.log(hello);
+const { text } = require('node:stream/consumers');
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+
+// console.log(textIn);
+// console.log(textOut);
