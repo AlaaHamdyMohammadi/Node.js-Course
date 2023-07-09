302d5027dd84ab5ef0dfd406790f04045a978732
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-09 16:15:08 +0200

    update

--- Section2(node-farm)/starter/index.js
+++ Section2(node-farm)/starter/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const fs = require('fs');
+const http = require('http');
+
+////////////////////////////////////
+//Files
 
 /*
 // Blocking, synchronus way
@@ -13,20 +17,34 @@ fs.writeFileSync('./txt/output.txt', textOut);
 // console.log(textOut);
 */
 
+/*
 // Non-Blocking, Asynchronus way
 fs.readFile('./txt/startttt.txt', 'utf-8', (err, data1) => {
     if(err) return console.log('Error ⛔');
     console.log(data1);
     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-      console.log(data2);
-      fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-        console.log(data3);
-
-        fs.writeFile('./txt/final.txt', `${data2} \n ${data3}` , 'utf-8', err => {
-            console.log('Your file has been written 🎉');
-        })
-      });
+        console.log(data2);
+        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+            console.log(data3);
+            
+            fs.writeFile('./txt/final.txt', `${data2} \n ${data3}` , 'utf-8', err => {
+                console.log('Your file has been written 🎉');
+            })
+        });
     });
 });
 console.log('Will read file !'); // executed before line 18
+*/
 
+
+////////////////////////////////////
+//Server
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server !')
+});
+
+server.listen(8000, '127.0.0.1', () =>{
+    console.log('Listening to request on port 8000')
+})
