ad7994036b33e385ccaa854e016d54a2a1651ecf
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-09 15:40:03 +0200

    update

--- Section2(node-farm)/starter/index.js
+++ Section2(node-farm)/starter/index.js
@@ -1,12 +1,32 @@
 'use strict';
 
 const fs = require('fs');
-const { text } = require('node:stream/consumers');
+
+/*
+// Blocking, synchronus way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
-
 // console.log(textIn);
 // console.log(textOut);
+*/
+
+// Non-Blocking, Asynchronus way
+fs.readFile('./txt/startttt.txt', 'utf-8', (err, data1) => {
+    if(err) return console.log('Error ⛔');
+    console.log(data1);
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+      console.log(data2);
+      fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+        console.log(data3);
+
+        fs.writeFile('./txt/final.txt', `${data2} \n ${data3}` , 'utf-8', err => {
+            console.log('Your file has been written 🎉');
+        })
+      });
+    });
+});
+console.log('Will read file !'); // executed before line 18
+
