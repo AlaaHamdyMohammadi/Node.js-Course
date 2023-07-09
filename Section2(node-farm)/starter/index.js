200eef57caaa7f1bd1ef3889ea1917af55267def
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-09 17:57:43 +0200

    update

--- Section2(node-farm)/starter/index.js
+++ Section2(node-farm)/starter/index.js
@@ -42,6 +42,10 @@ console.log('Will read file !'); // executed before line 18
 ////////////////////////////////////
 //Server
 
+//Converted to block code in the top level to execute only once
+const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     console.log(req.url);
     const pathName = req.url;
@@ -50,12 +54,8 @@ const server = http.createServer((req, res) => {
     }else if(pathName === '/product'){
         res.end('This is the product !');
     }else if(pathName === '/api'){
-        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) =>{
-            const productData = JSON.parse(data);
-            console.log(productData);
-            res.writeHead(200, {'Content-type' : 'application/json'})
-            res.end(data);
-        });
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
     }else{
         res.writeHead(404, {
             'Content-type': 'text/html',
