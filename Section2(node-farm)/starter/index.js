2b3c037b056bd8c875c535e9b7acdcf76a6fe3ee
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-09 17:02:30 +0200

    update

--- Section2(node-farm)/starter/index.js
+++ Section2(node-farm)/starter/index.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
+const url = require('url');
 
 ////////////////////////////////////
 //Files
@@ -41,10 +43,23 @@ console.log('Will read file !'); // executed before line 18
 //Server
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server !')
+    console.log(req.url);
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview !');
+    }else if(pathName === '/product'){
+        res.end('This is the product !');
+    }else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        })
+        res.end("<h1>Page not found !</h1>");
+    }
 });
 
 server.listen(8000, '127.0.0.1', () =>{
     console.log('Listening to request on port 8000')
-})
+});
+
+////////////////////////////////////
