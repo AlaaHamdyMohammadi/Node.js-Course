c9baea155c801bac7033f85c381005d23f05d50a
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-11 17:50:07 +0200

    update

---
+++ Section4/starter/event-loop.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+
+setTimeout(() => console.log('Timer 1 finish'),0);
+setImmediate(() => console.log("Immediate 1 finish"));
+
+fs.readFile('test-file.txt', () => {
+    console.log('I/O finished');
+    console.log('------------------')
+    setTimeout(() => console.log("Timer 2 finish"), 0);
+    setTimeout(() => console.log("Timer 3 finish"), 3000);
+    setImmediate(() => console.log("Immediate 2 finish"));
+    process.nextTick(() => console.log('process.nextTick'));
+
+    crypto.pbkdf2('password', 'alt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start ,'password encrypted')
+    })
+})
+console.log('Hello top-level-code')
